f0b2d905a9ad5fbf13e5df027365930eb8bb99e8
DiegoCorrea07 <diego.correa.cumbicus07@outlook.com>
2025-05-08 20:52:59 -0500

    Configurar la conexión a base de datos.

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,7 +1,14 @@
-import asyncpg
 import os
+from peewee import PostgresqlDatabase
+from urllib.parse import urlparse
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:pass@host:port/dbname")
+url = urlparse(DATABASE_URL)
 
-async def connect_db():
-    return await asyncpg.connect(DATABASE_URL)
+db = PostgresqlDatabase(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
