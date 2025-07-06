e03f1226b1601eae53d9a6391aa200abdea20077
diego <diego.correa.cumbicus07@outlook.com>
2025-07-06 16:47:31 -0500

    Actualización base de datos

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,11 +1,14 @@
+import asyncpg
+import os
 from peewee import PostgresqlDatabase
 from urllib.parse import urlparse
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://core_db_bcx4_user:IsHIQ3x7IOe3xL88Z9txFzXrYdFyJijg@dpg-d1f0dl6r433s73fnnth0-a.oregon-postgres.render.com/core_db_bcx4")
-
+DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:pass@host:port/dbname")
 url = urlparse(DATABASE_URL)
 
+async def connect_db():
+    return await asyncpg.connect(DATABASE_URL)
 db = PostgresqlDatabase(
     database=url.path[1:],
     user=url.username,
--- backend/utils/config.py
+++ backend/utils/config.py
@@ -2,4 +2,5 @@
 import os
 
 class Config:
+    SECRET_KEY = os.environ["SECRET_KEY"]
     SECRET_KEY = os.getenv("SECRET_KEY", "AdminCoreSecretKey")
